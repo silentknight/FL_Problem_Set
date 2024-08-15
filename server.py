88b697c6d02a1a1447436cd6db2c834fe1105f33
silentknight <1491628+silentknight@users.noreply.github.com>
2024-08-15 21:15:51 +0200

    Up

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ import numpy as np
 def main():
     # Define strategy
     class ModelStrategy(fl.server.strategy.FedAvg):
-        fraction_fit=0.5,
-        fraction_evaluate=0.5,
-        min_fit_clients=50,
-        min_evaluate_clients=50,
-        min_available_clients=100
+        fraction_fit=1.0,
+        fraction_evaluate=0.0,
+        min_fit_clients=1,
+        min_evaluate_clients=1,
+        min_available_clients=50
 
         def aggregate_fit(self, rnd, results, failures):
             aggregated_weights = super().aggregate_fit(rnd, results, failures)
